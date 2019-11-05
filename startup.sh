cf133ee608b9f4b62fc9c276ce06080742885152
fsnengineering <57364024+fsnengineering@users.noreply.github.com>
2019-11-05 12:24:30 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,11 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
+    mkdir /etc/opt/microsoft
+    mkdir /etc/opt/microsoft/omsagent
+    mkdir /etc/opt/microsoft/omsagent/$1
+    mkdir /etc/opt/microsoft/omsagent/$1/conf
+    mkdir /etc/opt/microsoft/omsagent/$1/conf/omsagent.d
     cd /usr/src/app
     wget https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/cef_installer.py
     sudo python /usr/src/app/cef_installer.py $1 $2
