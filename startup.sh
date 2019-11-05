9d2c469f17c052ec9c9c7fea339094bcbf25e775
fsnengineering <57364024+fsnengineering@users.noreply.github.com>
2019-11-05 11:25:33 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
+    cd /usr/src/app
+    wget https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/cef_installer.py
     sudo python /usr/src/app/cef_installer.py ${WORKSPACE_ID} ${PRIMARY_KEY}
 else
     echo "-- Not first container startup --"
